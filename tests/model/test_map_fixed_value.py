fc8329beac59916ebabbea0ebfbb8b7df9c34495
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-01-05 12:02:18 +0100

    Change model tests

--- tests/model/test_map_fixed_value.py
+++ tests/model/test_map_fixed_value.py
@@ -13,37 +13,42 @@ def value():
     return "C"
 
 
+@pytest.fixture()
+def located_in():
+    return "source"
+
+
 def test_default_instantiation(target, value):
     m = FixedValueMap(target, value)
 
     assert m.target == target
     assert m.value == value
-    assert m.is_fixed is True
+    assert m.located_in == "target"
 
 
-def test_full_instantiation(target, value):
-    m = FixedValueMap(target, value, False)
+def test_full_instantiation(target, value, located_in):
+    m = FixedValueMap(target, value, located_in)
 
     assert m.target == target
     assert m.value == value
-    assert m.is_fixed is False
+    assert m.located_in == located_in
 
 
 def test_immutable(target, value):
     m = FixedValueMap(target, value)
     with pytest.raises(AttributeError):
-        m.is_fixed = False
+        m.located_in = "source"
 
 
-def test_equal(target, value):
-    m1 = FixedValueMap(target, value)
-    m2 = FixedValueMap(target, value)
+def test_equal(target, value, located_in):
+    m1 = FixedValueMap(target, value, located_in)
+    m2 = FixedValueMap(target, value, located_in)
 
     assert m1 == m2
 
 
-def test_not_equal(target, value):
-    m1 = FixedValueMap(target, value)
-    m2 = FixedValueMap(target, value, False)
+def test_not_equal(target, value, located_in):
+    m1 = FixedValueMap(target, value, located_in)
+    m2 = FixedValueMap(target, value + value, located_in)
 
     assert m1 != m2
