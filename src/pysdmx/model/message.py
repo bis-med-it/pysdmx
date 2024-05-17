6357096f2b1229f2f1190a926d42dca84f52a25e
Francisco Javier Hernández del Caño <javier.hernandez@meaningfuldata.eu>
2024-05-17 12:03:52 +0000

    Added timezone.utc to default value in Header.prepared

--- src/pysdmx/model/message.py
+++ src/pysdmx/model/message.py
@@ -4,7 +4,7 @@ This module contains the enumeration for the different types of messages that
 can be written.
 """
 
-from datetime import datetime
+from datetime import datetime, timezone
 from enum import Enum
 from typing import Optional
 import uuid
@@ -29,7 +29,7 @@ class Header(Struct, frozen=True, kw_only=True):
 
     id: str = str(uuid.uuid4())
     test: bool = True
-    prepared: datetime = datetime.now()
+    prepared: datetime = datetime.now(timezone.utc)
     sender: str = "ZZZ"
     receiver: Optional[str] = None
     source: Optional[str] = None
