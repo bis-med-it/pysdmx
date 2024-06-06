6c9f99dbb97b9ba8d4df42056e991e104a01d05f
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-06-06 14:30:54 +0200

    Simplify SDMX-JSON message readers file

--- src/pysdmx/io/json/sdmxjson2/reader/__init__.py
+++ src/pysdmx/io/json/sdmxjson2/reader/__init__.py
@@ -1,38 +1,19 @@
 """Collection of readers for SDMX-JSON messages."""
 
 from pysdmx.fmr.reader import Deserializers
-from pysdmx.io.json.sdmxjson2.messages.category import (
-    JsonCategorySchemeMessage,
-)
-from pysdmx.io.json.sdmxjson2.messages.code import (
-    JsonCodelistMessage,
-    JsonHierarchyAssociationMessage,
-    JsonHierarchyMessage,
-)
-from pysdmx.io.json.sdmxjson2.messages.concept import JsonConcepSchemeMessage
-from pysdmx.io.json.sdmxjson2.messages.dataflow import JsonDataflowMessage
-from pysdmx.io.json.sdmxjson2.messages.map import (
-    JsonMappingMessage,
-    JsonRepresentationMapMessage,
-)
-from pysdmx.io.json.sdmxjson2.messages.org import (
-    JsonAgencyMessage,
-    JsonProviderMessage,
-)
-from pysdmx.io.json.sdmxjson2.messages.report import JsonMetadataMessage
-from pysdmx.io.json.sdmxjson2.messages.schema import JsonSchemaMessage
+from pysdmx.io.json.sdmxjson2 import messages as msg
 
 deserializers = Deserializers(
-    agencies=JsonAgencyMessage,  # type: ignore[arg-type]
-    categories=JsonCategorySchemeMessage,  # type: ignore[arg-type]
-    codes=JsonCodelistMessage,  # type: ignore[arg-type]
-    concepts=JsonConcepSchemeMessage,  # type: ignore[arg-type]
-    dataflow=JsonDataflowMessage,  # type: ignore[arg-type]
-    providers=JsonProviderMessage,  # type: ignore[arg-type]
-    schema=JsonSchemaMessage,  # type: ignore[arg-type]
-    hier_assoc=JsonHierarchyAssociationMessage,  # type: ignore[arg-type]
-    hierarchy=JsonHierarchyMessage,  # type: ignore[arg-type]
-    report=JsonMetadataMessage,  # type: ignore[arg-type]
-    mapping=JsonMappingMessage,  # type: ignore[arg-type]
-    code_map=JsonRepresentationMapMessage,  # type: ignore[arg-type]
+    agencies=msg.JsonAgencyMessage,  # type: ignore[arg-type]
+    categories=msg.JsonCategorySchemeMessage,  # type: ignore[arg-type]
+    codes=msg.JsonCodelistMessage,  # type: ignore[arg-type]
+    concepts=msg.JsonConcepSchemeMessage,  # type: ignore[arg-type]
+    dataflow=msg.JsonDataflowMessage,  # type: ignore[arg-type]
+    providers=msg.JsonProviderMessage,  # type: ignore[arg-type]
+    schema=msg.JsonSchemaMessage,  # type: ignore[arg-type]
+    hier_assoc=msg.JsonHierarchyAssociationMessage,  # type: ignore[arg-type]
+    hierarchy=msg.JsonHierarchyMessage,  # type: ignore[arg-type]
+    report=msg.JsonMetadataMessage,  # type: ignore[arg-type]
+    mapping=msg.JsonMappingMessage,  # type: ignore[arg-type]
+    code_map=msg.JsonRepresentationMapMessage,  # type: ignore[arg-type]
 )
