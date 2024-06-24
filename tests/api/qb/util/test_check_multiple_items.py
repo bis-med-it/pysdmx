113b8efe85fbd84f714cacf5d3573c65d42a2867
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-06-24 11:52:14 +0200

    Use array for multiple items

--- tests/api/qb/util/test_check_multiple_items.py
+++ tests/api/qb/util/test_check_multiple_items.py
@@ -8,4 +8,4 @@ def test_multiple_items():
     for v in ApiVersion:
         if v.value.number < ApiVersion.V1_3_0.value.number:
             with pytest.raises(ClientError):
-                check_multiple_items("A+B", v)
+                check_multiple_items(["A", "B"], v)
