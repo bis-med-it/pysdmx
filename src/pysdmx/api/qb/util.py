69215c54efdf9ae41aab0297669468a5c465ffa0
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-06-24 10:23:21 +0200

    Address QA checks

--- src/pysdmx/api/qb/util.py
+++ src/pysdmx/api/qb/util.py
@@ -1,13 +1,16 @@
+"""Utility classes for pysdmx REST query builders."""
+
 from enum import Enum
 import re
+
+from msgspec import Struct
 
 from pysdmx.errors import ClientError
 
 
-class _ApiVersion:
-    def __init__(self, label: str, number: int) -> None:
-        self.label = label
-        self.number = number
+class _ApiVersion(Struct, frozen=True):
+    label: str
+    number: int
 
     def __str__(self) -> str:
         return self.label
@@ -17,6 +20,8 @@ class _ApiVersion:
 
 
 class ApiVersion(Enum):
+    """The version of the SDMX-REST API."""
+
     V1_0_0 = _ApiVersion("V1.0.0", 0)
     V1_0_1 = _ApiVersion("V1.0.1", 1)
     V1_0_2 = _ApiVersion("V1.0.2", 2)
@@ -29,15 +34,19 @@ class ApiVersion(Enum):
     V2_1_0 = _ApiVersion("V2.1.0", 9)
 
     def __lt__(self, other: "ApiVersion") -> bool:
+        """Whether this version is less than the supplied one."""
         return self.value.number < other.value.number
 
     def __lte__(self, other: "ApiVersion") -> bool:
+        """Whether this version is less or equal to the supplied one."""
         return self.value.number <= other.value.number
 
     def __gt__(self, other: "ApiVersion") -> bool:
+        """Whether this version is greater than the supplied one."""
         return self.value.number > other.value.number
 
     def __gte__(self, other: "ApiVersion") -> bool:
+        """Whether this version is greater or equal to the supplied one."""
         return self.value.number >= other.value.number
 
 
@@ -45,6 +54,7 @@ MULT_SEP = re.compile(r"\+")
 
 
 def check_multiple_items(value: str, version: ApiVersion) -> None:
+    """Whether multiple items are supported in the supplied API version."""
     if version < ApiVersion.V1_3_0 and re.search(MULT_SEP, value):
         raise ClientError(
             422,
