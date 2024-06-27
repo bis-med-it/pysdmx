022be4a93cf4a622cab41594585589bf9f433844
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-06-27 15:03:10 +0200

    Improve test coverage

--- tests/api/qb/util/test_api_version.py
+++ tests/api/qb/util/test_api_version.py
@@ -30,4 +30,34 @@ def test_api_enum():
         assert v.value.label in expected
         if prev is not None:
             assert v.value.number > prev.value.number
+
+        prev = v
+
+
+def test_api_enum_comparisons():
+    expected = [
+        "V1.0.0",
+        "V1.0.1",
+        "V1.0.2",
+        "V1.1.0",
+        "V1.2.0",
+        "V1.3.0",
+        "V1.4.0",
+        "V1.5.0",
+        "V2.0.0",
+        "V2.1.0",
+    ]
+
+    assert len(ApiVersion) == 10
+    prev = None
+    for v in ApiVersion:
+        assert v.value.label in expected
+        if prev is not None:
+            assert v > prev
+            assert v >= prev
+            assert v >= v
+            assert v == v
+            assert prev < v
+            assert prev <= v
+
         prev = v
