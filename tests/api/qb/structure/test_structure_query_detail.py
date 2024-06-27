7f7048cbe206f31b829b710c34b269f4f16efb5a
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-06-27 10:33:24 +0200

    Add tests for details

---
+++ tests/api/qb/structure/test_structure_query_detail.py
@@ -0,0 +1,229 @@
+from typing import List
+
+import pytest
+
+from pysdmx.api.qb.structure import (
+    StructureDetail,
+    StructureQuery,
+    StructureReference,
+    StructureType,
+)
+from pysdmx.api.qb.util import ApiVersion
+from pysdmx.errors import ClientError
+
+
+details_initial = [
+    StructureDetail.FULL,
+    StructureDetail.ALL_STUBS,
+    StructureDetail.REFERENCE_STUBS,
+]
+
+
+details_1_3_0 = [
+    StructureDetail.REFERENCE_PARTIAL,
+    StructureDetail.REFERENCE_COMPLETE_STUBS,
+    StructureDetail.ALL_COMPLETE_STUBS,
+]
+
+
+details_2_0_0 = [StructureDetail.RAW]
+
+
+@pytest.fixture
+def typ():
+    return StructureType.DATAFLOW
+
+
+@pytest.fixture
+def agency():
+    return "BIS"
+
+
+@pytest.fixture
+def res():
+    return "CBS"
+
+
+@pytest.fixture
+def version():
+    return "1.0"
+
+
+@pytest.fixture
+def refs():
+    return StructureReference.NONE
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v < ApiVersion.V2_0_0)
+)
+@pytest.mark.parametrize("detail", details_initial)
+def test_url_core_details_before_2_0_0(
+    typ: StructureType,
+    agency: str,
+    res: str,
+    version: str,
+    refs: StructureReference,
+    detail: StructureDetail,
+    api_version: ApiVersion,
+):
+    expected = (
+        f"/{typ.value}/{agency}/{res}/{version}"
+        f"?detail={detail.value}&references={refs.value}"
+    )
+
+    q = StructureQuery(
+        typ, agency, res, version, detail=detail, references=refs
+    )
+    url = q.get_url(api_version)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v >= ApiVersion.V2_0_0)
+)
+@pytest.mark.parametrize("detail", details_initial)
+def test_url_core_details_since_2_0_0(
+    typ: StructureType,
+    agency: str,
+    res: str,
+    version: str,
+    refs: StructureReference,
+    detail: StructureDetail,
+    api_version: ApiVersion,
+):
+    expected = (
+        f"/structure/{typ.value}/{agency}/{res}/{version}"
+        f"?detail={detail.value}&references={refs.value}"
+    )
+
+    q = StructureQuery(
+        typ, agency, res, version, detail=detail, references=refs
+    )
+    url = q.get_url(api_version)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v >= ApiVersion.V2_0_0)
+)
+@pytest.mark.parametrize("detail", details_1_3_0)
+def test_url_1_3_0_details_since_2_0_0(
+    typ: StructureType,
+    agency: str,
+    res: str,
+    version: str,
+    refs: StructureReference,
+    detail: StructureDetail,
+    api_version: ApiVersion,
+):
+    expected = (
+        f"/structure/{typ.value}/{agency}/{res}/{version}"
+        f"?detail={detail.value}&references={refs.value}"
+    )
+
+    q = StructureQuery(
+        typ, agency, res, version, detail=detail, references=refs
+    )
+    url = q.get_url(api_version)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version",
+    (
+        v
+        for v in ApiVersion
+        if v >= ApiVersion.V1_3_0 and v < ApiVersion.V2_0_0
+    ),
+)
+@pytest.mark.parametrize("detail", details_1_3_0)
+def test_url_1_3_0_details_before_2_0_0(
+    typ: StructureType,
+    agency: str,
+    res: str,
+    version: str,
+    refs: StructureReference,
+    detail: StructureDetail,
+    api_version: ApiVersion,
+):
+    expected = (
+        f"/{typ.value}/{agency}/{res}/{version}"
+        f"?detail={detail.value}&references={refs.value}"
+    )
+
+    q = StructureQuery(
+        typ, agency, res, version, detail=detail, references=refs
+    )
+    url = q.get_url(api_version)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v >= ApiVersion.V2_0_0)
+)
+@pytest.mark.parametrize("detail", details_2_0_0)
+def test_url_2_0_0_details_since_2_0_0(
+    typ: StructureType,
+    agency: str,
+    res: str,
+    version: str,
+    refs: StructureReference,
+    detail: StructureDetail,
+    api_version: ApiVersion,
+):
+    expected = (
+        f"/structure/{typ.value}/{agency}/{res}/{version}"
+        f"?detail={detail.value}&references={refs.value}"
+    )
+
+    q = StructureQuery(
+        typ, agency, res, version, detail=detail, references=refs
+    )
+    url = q.get_url(api_version)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v < ApiVersion.V2_0_0)
+)
+@pytest.mark.parametrize("detail", details_2_0_0)
+def test_url_2_0_0_details_before_2_0_0(
+    typ: StructureType,
+    agency: str,
+    res: str,
+    version: str,
+    refs: StructureReference,
+    detail: StructureDetail,
+    api_version: ApiVersion,
+):
+    with pytest.raises(ClientError):
+        q = StructureQuery(
+            typ, agency, res, version, detail=detail, references=refs
+        )
+        q.get_url(api_version)
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v < ApiVersion.V1_3_0)
+)
+@pytest.mark.parametrize("detail", details_1_3_0)
+def test_url_1_3_0_details_before_1_3_0(
+    typ: StructureType,
+    agency: str,
+    res: str,
+    version: str,
+    refs: StructureReference,
+    detail: StructureDetail,
+    api_version: ApiVersion,
+):
+    with pytest.raises(ClientError):
+        q = StructureQuery(
+            typ, agency, res, version, detail=detail, references=refs
+        )
+        q.get_url(api_version)
