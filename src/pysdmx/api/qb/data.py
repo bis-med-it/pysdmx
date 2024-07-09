736022c15735e1ae2f63e94a777b5163b79ef80c
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-07-09 14:29:07 +0200

    Add basic data query builder

--- src/pysdmx/api/qb/data.py
+++ src/pysdmx/api/qb/data.py
@@ -1,7 +1,8 @@
 """Build SDMX-REST data queries."""
 
+from datetime import datetime
 from enum import Enum
-from typing import Optional
+from typing import Any, Dict, Optional
 
 import msgspec
 
@@ -15,6 +16,7 @@ class DataContext(Enum):
     DATA_STRUCTURE = "datastructure"
     DATAFLOW = "dataflow"
     PROVISION_AGREEMENT = "provisionagreement"
+    ALL = REST_ALL
 
 
 class DataFormat(Enum):
@@ -35,3 +37,83 @@ class DataFormat(Enum):
         "application/vnd.sdmx.structurespecifictimeseriesdata+xml;version=2.1"
     )
     SDMX_ML_3_0 = "application/vnd.sdmx.data+xml;version=3.0.0"
+
+
+class DataQuery(msgspec.Struct, frozen=True, omit_defaults=True):
+    """A data query.
+
+    Data queries allow retrieving statistical data.
+
+    Attributes:
+        context: The context for which data must be retrieved.
+        agency_id: The agency maintaining the context to be considered.
+        resource_id: The id of the context to be considered.
+        version: The version of the context to be considered.
+        key: The combination of dimension values identifying the slice
+            of the cube for which data should be returned. Wildcarding
+            is supported via the * operator.
+        components: The component values to be used for data filtering.
+        updated_after: The last time the query was performed by the client.
+        first_n_obs: The maximum number of observations to be returned for
+            each of the matching series, starting from the first observation.
+        last_n_obs: The maximum number of observations to be returned for
+            each of the matching series, counting back from the most recent
+            observation.
+        obs_dim: The ID of the dimension to be attached at the observation
+            level.
+        attributes: The attributes to be returned. Possible options are:
+            `dsd` (all the attributes defined in the DSD), `msd` (all the
+            reference metadata attributes), `dataset` (all the attributes
+            attached to the dataset-level), `series` (all the attributes
+            attached to the series-level), `obs` (all the attributes
+            attached to the observation-level), `all` (all attributes),
+            `none` (no attributes), {attribute_id}: The ID of the one or
+            more attributes to be returned.
+        measures: The measures to be returned. Possible options are:
+            `all` (all measures), `none` (no measure), {measure_id}:
+            The ID of the one or more measures to be returned.
+        include_history: Retrieve previous versions of the data, as they
+            were disseminated in the past.
+    """
+
+    context: DataContext = DataContext.ALL
+    agency_id: str = REST_ALL
+    resource_id: str = REST_ALL
+    version: str = REST_ALL
+    key: str = REST_ALL
+    components: Dict[str, Any] = None
+    updated_after: Optional[datetime] = None
+    first_n_obs: Optional[int] = None
+    last_n_obs: Optional[int] = None
+    obs_dimension: Optional[str] = None
+    attributes: str = "dsd"
+    measures: str = "all"
+    include_history: bool = False
+
+    def validate(self) -> None:
+        """Validate the query."""
+        try:
+            decoder.decode(encoder.encode(self))
+        except msgspec.DecodeError as err:
+            raise ClientError(422, "Invalid Schema Query", str(err)) from err
+
+    def get_url(self, version: ApiVersion, omit_defaults: bool = False) -> str:
+        """The URL for the query in the selected SDMX-REST API version."""
+        self.__validate_query(version)
+        if omit_defaults:
+            return self.__create_short_query(version)
+        else:
+            return self.__create_full_query(version)
+
+    def __validate_query(self, version: ApiVersion) -> None:
+        self.validate()
+
+    def __create_full_query(self, ver: ApiVersion) -> str:
+        return "/data"
+
+    def __create_short_query(self, ver: ApiVersion) -> str:
+        return "/data"
+
+
+decoder = msgspec.json.Decoder(DataQuery)
+encoder = msgspec.json.Encoder()
