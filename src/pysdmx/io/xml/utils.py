4e3a8df87b9e6fb6dc3923237e310a0954e31d22
javier.hernandez <javier.hernandez@meaningfuldata.eu>
2024-07-09 15:29:05 +0200

    Linting changes.
    Signed-off-by: javier.hernandez <javier.hernandez@meaningfuldata.eu>

--- src/pysdmx/io/xml/utils.py
+++ src/pysdmx/io/xml/utils.py
@@ -1,9 +1,11 @@
 """Utility functions for XML parsing and serialization."""
+
 from typing import Any, List
 
 
 def add_list(element: Any) -> List[Any]:
     """Make sure an element is a list and convert it if it is not.
+
     The xmltodict library always returns a list if there are multiple elements
     but a dict if there is only one element. This function makes sure that the
     element is always a list.
