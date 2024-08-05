e2daa65e0fd04fe134209a35e95e0ba955a57481
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-08-05 16:03:35 +0200

    Temporarily remove tests to find issue

--- tests/api/qb/refmeta/test_refmeta_detail.py
+++
@@ -1,9 +0,0 @@
-from pysdmx.api.qb.refmeta import RefMetaDetail
-
-
-def test_expected_details():
-    expected = ["full", "allstubs"]
-
-    assert len(RefMetaDetail) == len(expected)
-    for d in RefMetaDetail:
-        assert d.value in expected
