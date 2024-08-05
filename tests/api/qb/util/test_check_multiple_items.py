deb75db61d2e47d38ab282db7bfed6adc7911fb0
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-08-05 17:17:45 +0200

    Improve test

--- tests/api/qb/util/test_check_multiple_items.py
+++ tests/api/qb/util/test_check_multiple_items.py
@@ -4,8 +4,15 @@ from pysdmx.api.qb.util import ApiVersion, check_multiple_items
 from pysdmx.errors import ClientError
 
 
-def test_multiple_items():
+def test_check_multiple_items():
     for v in ApiVersion:
         if v < ApiVersion.V1_3_0:
             with pytest.raises(ClientError):
                 check_multiple_items(["A", "B"], v)
+        else:
+            try:
+                check_multiple_items(["A", "B"], v)
+            except ClientError:
+                pytest.raises(
+                    "A ClientError was received but none was expected."
+                )
