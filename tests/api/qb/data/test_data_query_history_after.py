41e51895680607d5a7350cc7946ff3dca467e98f
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-08-05 16:38:13 +0200

    Add tests back after investigations

---
+++ tests/api/qb/data/test_data_query_history_after.py
@@ -0,0 +1,120 @@
+import pytest
+
+from pysdmx.api.qb.data import DataQuery
+from pysdmx.api.qb.util import ApiVersion
+from pysdmx.errors import ClientError
+
+
+@pytest.fixture()
+def res():
+    return "CBS"
+
+
+@pytest.fixture()
+def history():
+    return True
+
+
+@pytest.mark.parametrize("api_version", ApiVersion)
+def test_invalid_value(res: str, api_version: ApiVersion):
+    q = DataQuery(resource_id=res, include_history=42)
+
+    with pytest.raises(ClientError):
+        q.get_url(api_version)
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v < ApiVersion.V2_0_0)
+)
+def test_url_history_before_2_0_0(
+    res: str,
+    history: bool,
+    api_version: ApiVersion,
+):
+    expected = (
+        f"/data/all,{res},latest/all"
+        f"?detail=full&includeHistory={str(history).lower()}"
+    )
+
+    q = DataQuery(resource_id=res, include_history=history)
+    url = q.get_url(api_version)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v >= ApiVersion.V2_0_0)
+)
+def test_url_history_since_2_0_0(
+    history: bool,
+    api_version: ApiVersion,
+):
+    expected = (
+        f"/data/*/*/*/*/*"
+        f"?attributes=dsd&measures=all&includeHistory={str(history).lower()}"
+    )
+
+    q = DataQuery(include_history=history)
+    url = q.get_url(api_version)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v < ApiVersion.V2_0_0)
+)
+def test_url_history_before_2_0_0_short(
+    res: str,
+    history: bool,
+    api_version: ApiVersion,
+):
+    expected = f"/data/{res}?includeHistory={str(history).lower()}"
+
+    q = DataQuery(resource_id=res, include_history=history)
+    url = q.get_url(api_version, True)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v >= ApiVersion.V2_0_0)
+)
+def test_url_history_since_2_0_0_short(
+    history: bool,
+    api_version: ApiVersion,
+):
+    expected = f"/data?includeHistory={str(history).lower()}"
+
+    q = DataQuery(include_history=history)
+    url = q.get_url(api_version, True)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v < ApiVersion.V2_0_0)
+)
+def test_url_default_history_before_2_0_0_short(
+    res: str,
+    api_version: ApiVersion,
+):
+    expected = f"/data/{res}"
+
+    q = DataQuery(resource_id=res)
+    url = q.get_url(api_version, True)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v >= ApiVersion.V2_0_0)
+)
+def test_url_default_history_since_2_0_0_short(
+    api_version: ApiVersion,
+):
+    expected = "/data"
+
+    q = DataQuery()
+    url = q.get_url(api_version, True)
+
+    assert url == expected
