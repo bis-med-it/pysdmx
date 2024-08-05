75bbdaae561c2c108e9377405c2745ca47deb43c
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-08-05 17:11:29 +0200

    Improve comparison

--- tests/api/qb/util/test_check_multiple_items.py
+++ tests/api/qb/util/test_check_multiple_items.py
@@ -6,6 +6,6 @@ from pysdmx.errors import ClientError
 
 def test_multiple_items():
     for v in ApiVersion:
-        if v.value.number < ApiVersion.V1_3_0.value.number:
+        if v < ApiVersion.V1_3_0:
             with pytest.raises(ClientError):
                 check_multiple_items(["A", "B"], v)
