7fc0c805e881266e88bc0cba2cf19725b1afa4bd
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-08-06 11:47:30 +0200

    Keep one test only

--- tests/api/qb/util/test_api_version.py
+++ tests/api/qb/util/test_api_version.py
@@ -20,30 +20,30 @@ def test_api_enum():
         assert v.value in expected
 
 
-def test_api_enum_comparisons():
-    expected = [
-        "V1.0.0",
-        "V1.0.1",
-        "V1.0.2",
-        "V1.1.0",
-        "V1.2.0",
-        "V1.3.0",
-        "V1.4.0",
-        "V1.5.0",
-        "V2.0.0",
-        "V2.1.0",
-    ]
+# def test_api_enum_comparisons():
+#     expected = [
+#         "V1.0.0",
+#         "V1.0.1",
+#         "V1.0.2",
+#         "V1.1.0",
+#         "V1.2.0",
+#         "V1.3.0",
+#         "V1.4.0",
+#         "V1.5.0",
+#         "V2.0.0",
+#         "V2.1.0",
+#     ]
 
-    assert len(ApiVersion) == 10
-    prev = None
-    for v in ApiVersion:
-        assert v.value in expected
-        if prev is not None:
-            assert v > prev
-            assert v >= prev
-            assert v >= v
-            assert v == v
-            assert prev < v
-            assert prev <= v
+#     assert len(ApiVersion) == 10
+#     prev = None
+#     for v in ApiVersion:
+#         assert v.value in expected
+#         if prev is not None:
+#             assert v > prev
+#             assert v >= prev
+#             assert v >= v
+#             assert v == v
+#             assert prev < v
+#             assert prev <= v
 
-        prev = v
+#         prev = v
