a4f685843846c95e953221a6905ebcf2435ae24d
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-09-26 12:07:21 +0200

    Add missing properties

--- src/pysdmx/io/json/sdmxjson2/messages/pa.py
+++ src/pysdmx/io/json/sdmxjson2/messages/pa.py
@@ -1,10 +1,24 @@
 """Collection of SDMX-JSON schemas for provision agreements."""
 
+from datetime import datetime
+from typing import Optional, Sequence
+
 from msgspec import Struct
+
+from pysdmx.io.json.sdmxjson2.messages.core import JsonAnnotation
 
 
 class JsonProvisionAgreement(Struct, frozen=True):
     """SDMX-JSON payload for a provision agreement."""
 
+    id: str
+    name: str
+    agency: str
+    description: Optional[str] = None
+    version: str = "1.0"
+    isExternalReference: bool = False
+    validFrom: Optional[datetime] = None
+    validTo: Optional[datetime] = None
+    annotations: Sequence[JsonAnnotation] = None
     structureUsage: str
     dataProvider: str
