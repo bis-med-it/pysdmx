051959a866ab36e8f2a0a9dc2fc0c09926e5355b
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-11-19 10:22:13 +0100

    minValue and maxValue are strings in fusion-json

--- src/pysdmx/io/json/fusion/messages/core.py
+++ src/pysdmx/io/json/fusion/messages/core.py
@@ -35,8 +35,8 @@ class FusionTextFormat(msgspec.Struct, frozen=True):
     textType: str
     minLength: Optional[int] = None
     maxLength: Optional[int] = None
-    minValue: Optional[Union[int, float]] = None
-    maxValue: Optional[Union[int, float]] = None
+    minValue: Optional[Union[str]] = None
+    maxValue: Optional[Union[str]] = None
     startValue: Optional[Union[int, float]] = None
     endValue: Optional[Union[int, float]] = None
     decimals: Optional[int] = None
