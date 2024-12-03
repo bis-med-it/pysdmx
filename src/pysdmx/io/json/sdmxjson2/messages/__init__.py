7649ff21386336d9c4f9931ebe3a279f34df9104
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-12-03 16:27:45 +0100

    Address flake8 issues

--- src/pysdmx/io/json/sdmxjson2/messages/__init__.py
+++ src/pysdmx/io/json/sdmxjson2/messages/__init__.py
@@ -23,7 +23,9 @@ from pysdmx.io.json.sdmxjson2.messages.org import (
 )
 from pysdmx.io.json.sdmxjson2.messages.report import JsonMetadataMessage
 from pysdmx.io.json.sdmxjson2.messages.schema import JsonSchemaMessage
-from pysdmx.io.json.sdmxjson2.messages.vtl import JsonVtlTransformationsMessage
+from pysdmx.io.json.sdmxjson2.messages.vtl import (
+    JsonVtlTransformationsMessage as JsonTransfoMsg,
+)
 
 __all__ = [
     "JsonAgencyMessage",
@@ -39,5 +41,5 @@ __all__ = [
     "JsonMetadataMessage",
     "JsonMappingMessage",
     "JsonRepresentationMapMessage",
-    "JsonVtlTransformationsMessage",
+    "JsonTransfoMsg",
 ]
