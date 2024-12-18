57c694cd7b6f158f7fe46baddad7c69a08453c28
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2024-12-18 10:10:35 +0100

    Add tests

---
+++ tests/api/fmr/fusion/test_provision_agreement.py
@@ -0,0 +1,49 @@
+import pytest
+import tests.api.fmr.pa_checks as checks
+
+from pysdmx.api.fmr import AsyncRegistryClient, Format, RegistryClient
+
+
+@pytest.fixture()
+def fmr():
+    return RegistryClient(
+        "https://registry.sdmx.org/sdmx/v2/",
+        Format.FUSION_JSON,
+    )
+
+
+@pytest.fixture()
+def async_fmr() -> AsyncRegistryClient:
+    return AsyncRegistryClient(
+        "https://registry.sdmx.org/sdmx/v2",
+        Format.FUSION_JSON,
+    )
+
+
+@pytest.fixture()
+def query(fmr):
+    res = "/structure/provisionagreement/"
+    agency = "BIS.CBS"
+    id = "CBS_BIS_5B0"
+    return f"{fmr.api_endpoint}{res}{agency}/{id}/1.0"
+
+
+@pytest.fixture()
+def body():
+    with open("tests/api/fmr/samples/pa/pa.fusion.json", "rb") as f:
+        return f.read()
+
+
+def test_returns_provision_agreement(respx_mock, fmr, query, body):
+    """get_provision_agreement() should return a provision_agreement."""
+    checks.check_provision_agreements(respx_mock, fmr, query, body)
+
+
+@pytest.mark.asyncio()
+async def test_returns_provision_agreement_async(
+    respx_mock, async_fmr, query, body
+):
+    """get_provision_agreement() should return a provision_agreement (async)."""
+    await checks.check_provision_agreements_async(
+        respx_mock, async_fmr, query, body
+    )
