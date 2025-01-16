a2b02fd58951485b80136388ece21be9ed3e86a4
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2025-01-16 17:48:42 +0100

    Test all unsupported messages

--- tests/api/fmr/test_client.py
+++ tests/api/fmr/test_client.py
@@ -4,13 +4,18 @@ from pysdmx.api.fmr import Format, RegistryClient
 from pysdmx.errors import NotImplemented
 
 
-def test_unsupported_format():
+@pytest.mark.parametrize(
+    "fmt",
+    (
+        f
+        for f in Format
+        if f not in [Format.FUSION_JSON, Format.SDMX_JSON_2_0_0]
+    ),
+)
+def test_unsupported_format(fmt):
     with pytest.raises(NotImplemented) as ni:
-        RegistryClient(
-            "https://registry.sdmx.org/sdmx/v2",
-            Format.SDMX_JSON_1_0_0,
-        )
+        RegistryClient("https://registry.sdmx.org/sdmx/v2", fmt)
     assert ni.value.title is not None
     assert ni.value.description is not None
     assert "requested_format" in ni.value.csi
-    assert ni.value.csi["requested_format"] == Format.SDMX_JSON_1_0_0.value
+    assert ni.value.csi["requested_format"] == fmt.value
