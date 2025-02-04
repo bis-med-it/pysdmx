bb82d5993be6940d2a23e494670f810d026c050c
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2025-02-04 11:32:14 +0100

    Add tests for registration queries by provider

---
+++ tests/api/qb/registration/test_regprv_query_updated_after.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timezone
+
+import pytest
+
+from pysdmx.api.qb.registration import RegistrationByProviderQuery
+from pysdmx.api.qb.util import ApiVersion
+
+
+@pytest.fixture
+def updated_after():
+    return datetime.now(timezone.utc)
+
+
+@pytest.mark.parametrize(
+    "api_version",
+    (v for v in ApiVersion if v >= ApiVersion.V2_1_0),
+)
+def test_url_no_default_updated_after(
+    api_version: ApiVersion,
+):
+    expected = f"/registration/provider/*/*"
+
+    q = RegistrationByProviderQuery()
+    url = q.get_url(api_version)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v >= ApiVersion.V2_1_0)
+)
+def test_url_updated_after(
+    updated_after: datetime,
+    api_version: ApiVersion,
+):
+    expected = (
+        "/registration/provider/*/*"
+        f'?updatedAfter={updated_after.isoformat("T", "seconds")}'
+    )
+
+    q = RegistrationByProviderQuery(updated_after=updated_after)
+    url = q.get_url(api_version)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v >= ApiVersion.V2_1_0)
+)
+def test_url_omit_defaults_updated_after(
+    updated_after: datetime, api_version: ApiVersion
+):
+    expected = (
+        "/registration/provider"
+        f'?updatedAfter={updated_after.isoformat("T", "seconds")}'
+    )
+
+    q = RegistrationByProviderQuery(updated_after=updated_after)
+    url = q.get_url(api_version, True)
+
+    assert url == expected
