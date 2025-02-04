7223c03e77b3352798d4a7201dd1ba33086583de
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2025-02-04 14:11:02 +0100

    Increase test coverage

--- tests/api/qb/registration/test_regid_query_id.py
+++ tests/api/qb/registration/test_regid_query_id.py
@@ -46,6 +46,21 @@ def test_url_single_id(
 
 
 @pytest.mark.parametrize(
+    "api_version", (v for v in ApiVersion if v >= ApiVersion.V2_1_0)
+)
+def test_url_single_id_short(
+    id: str,
+    api_version: ApiVersion,
+):
+    expected = f"/registration/id/{id}"
+
+    q = RegistrationByIdQuery(id)
+    url = q.get_url(api_version, True)
+
+    assert url == expected
+
+
+@pytest.mark.parametrize(
     "api_version", (v for v in ApiVersion if v < ApiVersion.V2_1_0)
 )
 def test_registration_query_before_2_1_0(id: str, api_version: ApiVersion):
