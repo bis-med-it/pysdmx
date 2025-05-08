75776f67b06022d8dc5f2e8254786e095a054b92
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2025-05-08 10:01:48 +0200

    Rename tests

--- tests/util/test_parse_maintainable_urn.py
+++ tests/util/test_parse_maintainable_urn.py
@@ -2,18 +2,18 @@ import pytest
 
 from pysdmx.errors import Invalid
 from pysdmx.model import Reference
-from pysdmx.util import parse_urn
+from pysdmx.util import parse_maintainable_urn
 
 
 def test_no_match():
     with pytest.raises(Invalid):
-        parse_urn("test")
+        parse_maintainable_urn("test")
 
 
 def test_match():
     cl = "urn:sdmx:org.sdmx.infomodel.codelist.Codelist=SDMX:CL_FREQ(1.0)"
 
-    m = parse_urn(cl)
+    m = parse_maintainable_urn(cl)
 
     assert isinstance(m, Reference)
     assert m.sdmx_type == "Codelist"
