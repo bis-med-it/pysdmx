916dad71388cfd3fc26bd2b4653e8c2a6bdf9532
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-05-12 16:50:58 +0200

    Updated Gds Agency model.

--- src/pysdmx/model/gds.py
+++ src/pysdmx/model/gds.py
@@ -1,6 +1,8 @@
 from msgspec import Struct
 
-from pysdmx.model import Agency, AgencyScheme as AS
+from pysdmx.model import Agency
+
+from typing import Optional
 
 
 class GdsAgency(Struct, frozen=True):
@@ -16,10 +18,10 @@ class GdsAgency(Struct, frozen=True):
     agencyId: str
     name: str
     url: str
-    description: Optional[str] = None
+    description: str = ""
 
     def to_model(self, owner: Optional[str] = None) -> Agency:
         """Converts a GdsOrg to a standard Organisation."""
         d = self.description
-        oid = f"{owner}.{self.code}" if owner and owner != "SDMX" else self.code
+        oid = f"{owner}.{self.agencyId}" if owner and owner != "SDMX" else self.agencyId
         return Agency(id=oid, name=self.name, description=d, contacts=None)
