881b2a3dece0df25c5636c36667c0d85465b355e
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-05-13 12:41:18 +0200

    Added Gds Catalog deserializer.

---
+++ src/pysdmx/io/json/gds/messages/catalog.py
@@ -0,0 +1,17 @@
+from typing import Sequence
+from msgspec import Struct
+from pysdmx.model.gds import GdsCatalog
+
+
+class JsonStructures(Struct, frozen=True):
+    """Intermediate structure for 'structures' field."""
+    catalogs: Sequence[GdsCatalog]
+
+
+class JsonCatalogMessage(Struct, frozen=True):
+    """SDMX-JSON payload for /catalog queries."""
+    structures: JsonStructures
+
+    def to_model(self) -> Sequence[GdsCatalog]:
+        """Returns a list of GdsCatalog objects."""
+        return self.structures.catalogs
