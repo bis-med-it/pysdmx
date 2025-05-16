d8b6c5dc804679a19918ef05c996904f0989982d
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2025-05-16 15:50:44 +0200

    Add test for extraction of agency dataflows

---
+++ tests/io/json/sdmxjson2/deser/test_agencies_with_flows.py
@@ -0,0 +1,32 @@
+import msgspec
+import pytest
+
+from pysdmx.io.json.sdmxjson2.messages import JsonAgencyMessage
+from pysdmx.model import Agency, AgencyScheme
+
+
+@pytest.fixture
+def body():
+    with open(
+        "tests/io/json/sdmxjson2/deser/samples/orgs/agencies_flows.json", "rb"
+    ) as f:
+        return f.read()
+
+
+def test_annotation_with_uri(body):
+    res = msgspec.json.Decoder(JsonAgencyMessage).decode(body)
+    schemes = res.to_model()
+
+    assert len(schemes) == 1
+    scheme = schemes[0]
+    assert isinstance(scheme, AgencyScheme)
+    assert len(scheme) == 2
+    for agency in scheme:
+        assert isinstance(agency, Agency)
+        if agency.id == "BIS.DST":
+            assert len(agency.dataflows) == 0
+        else:
+            assert agency.id == "BIS.CMP"
+            assert len(agency.dataflows) == 2
+            for flow in agency.dataflows:
+                assert flow.id in ["TEST_ARRAYS_DF", "TEST_ARRAYS_DF_1"]
