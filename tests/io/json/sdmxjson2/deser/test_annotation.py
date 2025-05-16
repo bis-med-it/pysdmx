6c8a9ca3d4338b1d376e2f3254c8381b11fc9eff
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2025-05-16 13:46:00 +0200

    Improve test coverage

--- tests/io/json/sdmxjson2/deser/test_annotation.py
+++ tests/io/json/sdmxjson2/deser/test_annotation.py
@@ -6,17 +6,37 @@ from pysdmx.model import Codelist
 
 
 @pytest.fixture
-def body():
+def body_uri():
     with open(
-        "tests/io/json/sdmxjson2/deser/samples/annot/hier.json", "rb"
+        "tests/io/json/sdmxjson2/deser/samples/annot/hier_uri.json", "rb"
     ) as f:
         return f.read()
 
 
-def test_annotation_with_url(body):
+@pytest.fixture
+def body_href():
+    with open(
+        "tests/io/json/sdmxjson2/deser/samples/annot/hier_href.json", "rb"
+    ) as f:
+        return f.read()
+
+
+def test_annotation_with_uri(body_uri):
     exp = "https://test.org/code-validity"
 
-    res = msgspec.json.Decoder(JsonCodelistMessage).decode(body)
+    res = msgspec.json.Decoder(JsonCodelistMessage).decode(body_uri)
+    cl = res.to_model()
+
+    assert isinstance(cl, Codelist)
+    assert len(cl.annotations) == 1
+    a = cl.annotations[0]
+    assert a.url == exp
+
+
+def test_annotation_with_href(body_href):
+    exp = "https://test.org/code-validity"
+
+    res = msgspec.json.Decoder(JsonCodelistMessage).decode(body_href)
     cl = res.to_model()
 
     assert isinstance(cl, Codelist)
