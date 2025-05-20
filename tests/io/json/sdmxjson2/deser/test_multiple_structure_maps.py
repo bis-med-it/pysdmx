0b306fd59019f16fd0b37f6485644d0aefbe475e
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2025-05-20 13:55:00 +0200

    Check that representation maps can be a urn

--- tests/io/json/sdmxjson2/deser/test_multiple_structure_maps.py
+++ tests/io/json/sdmxjson2/deser/test_multiple_structure_maps.py
@@ -19,3 +19,15 @@ def test_multiple_structure_maps(body):
     for m in maps:
         assert isinstance(m, StructureMap)
         assert m.version in ["1.0", "1.1"]
+
+
+def test_representation_map_urn(body):
+    res = msgspec.json.Decoder(JsonStructureMapsMessage).decode(body)
+    maps = res.to_model()
+    exp = "urn:sdmx:org.sdmx.infomodel.structuremapping.RepresentationMap"
+
+    assert len(maps) == 2
+    for m in maps:
+        for cm in m.component_maps:
+            assert isinstance(cm.values, str)
+            assert cm.values.startswith(exp)
