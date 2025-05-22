3bf745d7980171a8eddb7df812bc6dfed75b3cac
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2025-05-22 15:00:40 +0200

    Add test for JsonHierarchiesMessage

---
+++ tests/io/json/sdmxjson2/deser/test_hierarchies.py
@@ -0,0 +1,23 @@
+import msgspec
+import pytest
+
+from pysdmx.io.json.sdmxjson2.messages import JsonHierarchiesMessage
+from pysdmx.model import Hierarchy
+
+
+@pytest.fixture
+def body():
+    with open(
+        "tests/io/json/sdmxjson2/deser/samples/hier/hier.json", "rb"
+    ) as f:
+        return f.read()
+
+
+def test_ha_deser(body):
+    res = msgspec.json.Decoder(JsonHierarchiesMessage).decode(body)
+    hierarchies = res.to_model()
+
+    assert len(hierarchies) == 2
+    for h in hierarchies:
+        assert isinstance(h, Hierarchy)
+        assert h.version in ["1.0", "1.42"]
