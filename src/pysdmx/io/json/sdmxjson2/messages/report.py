46af04faf53588ba20c4649d5b808271a8c58d05
Xavier Sosnovsky <xavier.sosnovsky@bis.org>
2025-05-27 10:53:50 +0200

    Add all missing properties to SDMX-JSON Metadata Report

--- src/pysdmx/io/json/sdmxjson2/messages/report.py
+++ src/pysdmx/io/json/sdmxjson2/messages/report.py
@@ -1,16 +1,64 @@
 """Collection of SDMX-JSON schemas for reference metadata queries."""
 
-from typing import Sequence
+from typing import Optional, Sequence
 
 from msgspec import Struct
 
-from pysdmx.model.metadata import MetadataReport, merge_attributes
+from pysdmx.io.json.sdmxjson2.messages.core import ItemSchemeType
+from pysdmx.model.dataset import ActionType
+from pysdmx.model.metadata import (
+    MetadataAttribute,
+    MetadataReport,
+    merge_attributes,
+)
+
+
+class JsonMetadataReport(ItemSchemeType, frozen=True):
+    """SDMX-JSON payload for a metadata report."""
+
+    metadataflow: str = ""
+    targets: Sequence[str] = ()
+    attributes: Sequence[MetadataAttribute] = ()
+    metadataProvisionAgreement: Optional[str] = None
+    publicationPeriod: Optional[str] = None
+    publicationYear: Optional[str] = None
+    reportingBegin: Optional[str] = None
+    reportingEnd: Optional[str] = None
+    action: Optional[str] = None
+
+    def to_model(self) -> MetadataReport:
+        """Converts a JsonMetadataReport to a standard report."""
+        attrs = merge_attributes(self.attributes)
+        return MetadataReport(
+            annotations=[a.to_model() for a in self.annotations],
+            id=self.id,
+            name=self.name,
+            description=self.description,
+            valid_from=self.validFrom,
+            valid_to=self.validTo,
+            version=self.version,
+            agency=self.agency,
+            is_external_reference=self.isExternalReference,
+            metadataflow=self.metadataflow,
+            targets=self.targets,
+            attributes=attrs,
+            metadataProvisionAgreement=self.metadataProvisionAgreement,
+            publicationPeriod=self.publicationPeriod,
+            publicationYear=self.publicationYear,
+            reportingBegin=self.reportingBegin,
+            reportingEnd=self.reportingEnd,
+            action=ActionType(self.action) if self.action else None,
+        )
 
 
 class JsonMetadataSets(Struct, frozen=True):
     """SDMX-JSON payload for the list of metadata sets."""
 
-    metadataSets: Sequence[MetadataReport]
+    metadataSets: Sequence[JsonMetadataReport]
+
+    def to_model(self) -> Sequence[MetadataReport]:
+        """Returns the requested metadata report(s)."""
+        return [r.to_model() for r in self.metadataSets]
 
 
 class JsonMetadataMessage(Struct, frozen=True):
@@ -18,15 +66,10 @@ class JsonMetadataMessage(Struct, frozen=True):
 
     data: JsonMetadataSets
 
-    def __create_report(self, r: MetadataReport) -> MetadataReport:
-        attrs = merge_attributes(r.attributes)
-        return MetadataReport(
-            r.id, r.name, r.metadataflow, r.targets, attrs, r.version
-        )
-
     def to_model(self, fetch_all: bool = False) -> Sequence[MetadataReport]:
         """Returns the requested metadata report(s)."""
+        reports = self.data.to_model()
         if fetch_all:
-            return [self.__create_report(r) for r in self.data.metadataSets]
+            return reports
         else:
-            return [self.__create_report(self.data.metadataSets[0])]
+            return [reports[0]]
