718875aa563f12ecefa7e7d6b15e8c6209f35ecd
javier.hernandez <73304358+javihern98@users.noreply.github.com>
2025-05-28 18:22:39 +0200

    Added prettyprint to generate_vtl_script. Fixed docstrings and access.
    Signed-off-by: javier.hernandez <73304358+javihern98@users.noreply.github.com>

--- src/pysdmx/toolkit/vtl/__init__.py
+++ src/pysdmx/toolkit/vtl/__init__.py
@@ -1,5 +1,6 @@
 """VTL toolkit for PySDMX."""
 
-from pysdmx.toolkit.vtl import generate_vtl_script, model_validations
+from pysdmx.toolkit.vtl.model_validations import model_validations
+from pysdmx.toolkit.vtl.script_generation import generate_vtl_script
 
 __all__ = ["model_validations", "generate_vtl_script"]
