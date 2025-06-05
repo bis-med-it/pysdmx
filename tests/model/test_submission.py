ad2b269844015fb51db5c3bde416d43b20cd6cdb
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-06-05 17:12:25 +0200

    Fixed some tests.

--- tests/model/test_submission.py
+++ tests/model/test_submission.py
@@ -24,4 +24,4 @@ def test_full_instantiation(action, short_urn, status):
     assert submission_result.action == action
     assert submission_result.short_urn == short_urn
     assert submission_result.status == status
-    assert str(submission_result) == 'action: Append, short_urn: Codelist=BIS:BIS_DER(1.0), status: Success'
+    assert str(submission_result) == f'action: {action}, short_urn: {short_urn}, status: {status}'
