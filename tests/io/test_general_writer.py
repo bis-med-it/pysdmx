d552c86729697eb8202dcb4b0d0d04604e734010
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-06-13 10:41:04 +0200

    Fixed general writer tests coverage on pysdmx.io.writer.

--- tests/io/test_general_writer.py
+++ tests/io/test_general_writer.py
@@ -6,7 +6,7 @@ import pytest
 from pysdmx.io import read_sdmx
 from pysdmx.io.format import Format
 from pysdmx.model import Component, Components, Concept, Role, Schema
-from src.pysdmx.io.writer import write
+from pysdmx.io.writer import write
 
 CSV_1_0_PATH = Path(__file__).parent / "csv" / "sdmx10" / "reader"
 CSV_2_0_PATH = Path(__file__).parent / "csv" / "sdmx20" / "reader"
