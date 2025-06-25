fbb40ab1e57cc1eb1631354cea01e5f0e5d56f0f
Javier <javier.hernandez@meaningfuldata.eu>
2025-06-25 12:23:56 +0200

    Moved tests to writing validation and added roundtrip test on attribute relationship to sdmx-ml 2.1 writer.

--- tests/io/xml/sdmx30/writer/test_writing_validation.py
+++ tests/io/xml/sdmx30/writer/test_writing_validation.py
@@ -4,7 +4,7 @@ import pytest
 
 from pysdmx.io import read_sdmx
 from pysdmx.io.xml.sdmx30.writer.structure import write
-
+from pysdmx.model import DataStructureDefinition
 
 @pytest.fixture
 def samples_folder():
@@ -29,3 +29,31 @@ def test_roundtrip(samples_folder, filename, tmp_path):
     msg2 = read_sdmx(result)
 
     assert len(msg1.structures) == len(msg2.structures)
+
+def test_attribute_relationship_roundtrip(samples_folder):
+    # Read the SDMX-ML file with None attribute relationships
+    file_path = samples_folder / "datastructure_att_rel_21.xml"
+    message = read_sdmx(file_path.read_text(), validate=True)
+
+    # Check we have it set to "D" (Dataflow)
+    structure_one: DataStructureDefinition = message.structures[0]
+    attribute_one = structure_one.components.attributes[0]
+    assert attribute_one.attachment_level == "D"
+
+    # Write it back to SDMX-ML format
+    result = write(
+        structures=message.structures,
+        prettyprint=True,
+    )
+    # Check if a str:Dataflow is present in the output
+    assert "<str:Dataflow/>" in result
+
+    # Read the written content back
+    read_message = read_sdmx(result, validate=True)
+
+    structure_two: DataStructureDefinition = read_message.structures[0]
+    attribute_two = structure_two.components.attributes[0]
+    # Check if the attribute relationships are Dataflow
+    assert attribute_two.attachment_level == "D"
+
+    assert attribute_one == attribute_two
