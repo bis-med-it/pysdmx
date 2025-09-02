c74c53a51991e8f5d612586af6c07ee3000470b7
Jose Franco <jose.franco@meaningfuldata.eu>
2025-09-02 17:19:08 +0200

    cr-384: add dc to extra tests

--- tests/api/dc/conftest.py
+++ tests/api/dc/conftest.py
@@ -7,5 +7,5 @@ import pytest
 def pytest_collection_modifyitems(items):
     for item in items:
         p = Path(str(item.fspath)).as_posix()
-        if "/tests/io/csv/" in p or p.endswith("/tests/io/csv"):
-            item.add_marker(pytest.mark.data)
+        if "/tests/api/dc/" in p or p.endswith("/tests/api/dc"):
+            item.add_marker(pytest.mark.dc)
