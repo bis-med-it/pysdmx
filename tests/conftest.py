ad36d192cd4bb165e00491a1b6af3ce6c604cfa2
Jose Franco <jose.franco@meaningfuldata.eu>
2025-09-02 17:35:34 +0200

    cr-384: fix conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,30 +2,20 @@ from pathlib import Path
 
 import pytest
 
+# Mapa carpeta → marker
+PATH_TO_MARK = {
+    "/tests/io/xml/": "xml",
+    "/tests/io/csv/": "data",
+    "/tests/api/dc/": "dc",
+    "/tests/toolkit/": "vtl",
+}
 
-def pytest_collection_xml(items):
+
+def pytest_collection_modifyitems(config, items):
+    root = Path(config.rootdir)
     for item in items:
-        p = Path(str(item.fspath)).as_posix()
-        if "/tests/io/xml/" in p or p.endswith("/tests/io/xml"):
-            item.add_marker(pytest.mark.xml)
-
-
-def pytest_collection_data(items):
-    for item in items:
-        p = Path(str(item.fspath)).as_posix()
-        if "/tests/io/csv/" in p or p.endswith("/tests/io/csv"):
-            item.add_marker(pytest.mark.data)
-
-
-def pytest_collection_dc(items):
-    for item in items:
-        p = Path(str(item.fspath)).as_posix()
-        if "/tests/api/dc/" in p or p.endswith("/tests/api/dc"):
-            item.add_marker(pytest.mark.dc)
-
-
-def pytest_collection_vtl(items):
-    for item in items:
-        p = Path(str(item.fspath)).as_posix()
-        if "/tests/toolkit/" in p or p.endswith("/tests/toolkit/"):
-            item.add_marker(pytest.mark.vtl)
+        rel = Path(item.fspath).resolve().relative_to(root).as_posix()
+        for subpath, markname in PATH_TO_MARK.items():
+            if subpath in f"/{rel}/":
+                item.add_marker(getattr(pytest.mark, markname))
+                break
