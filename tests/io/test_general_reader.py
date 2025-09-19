c3e323bdc344cb21a7c802a7b1af0830646363d3
Xavier Sosnovsky <1846109+sosna@users.noreply.github.com>
2025-09-19 11:07:56 +0200

    Add tests to read structures and reference metadata in SDMX-JSON 2.0.0

--- tests/io/test_general_reader.py
+++ tests/io/test_general_reader.py
@@ -8,7 +8,8 @@ import pysdmx.io.input_processor as m
 from pysdmx.errors import Invalid
 from pysdmx.io import read_sdmx
 from pysdmx.io.reader import get_datasets
-from pysdmx.model import Schema
+from pysdmx.model import Codelist, MetadataReport, Schema
+from pysdmx.model.message import Message
 
 
 @pytest.fixture
@@ -22,6 +23,36 @@ def empty_message():
 @pytest.fixture
 def sdmx_json():
     file_path = Path(__file__).parent / "samples" / "sdmx.json"
+    with open(file_path, "r") as f:
+        text = f.read()
+    return text
+
+
+@pytest.fixture
+def sdmx_json_structure():
+    file_path = (
+        Path(__file__).parent.parent
+        / "api"
+        / "fmr"
+        / "samples"
+        / "code"
+        / "freq.json"
+    )
+    with open(file_path, "r") as f:
+        text = f.read()
+    return text
+
+
+@pytest.fixture
+def sdmx_json_refmeta():
+    file_path = (
+        Path(__file__).parent.parent
+        / "api"
+        / "fmr"
+        / "samples"
+        / "refmeta"
+        / "report.json"
+    )
     with open(file_path, "r") as f:
         text = f.read()
     return text
@@ -338,3 +369,31 @@ def test_get_datasets_missing_attribute(samples_folder):
     }
     assert "DECIMALS" not in dataset.data.columns
     assert "UNIT_MULT" not in dataset.data.columns
+
+
+def test_get_json2_structure(sdmx_json_structure):
+    msg = read_sdmx(sdmx_json_structure)
+
+    assert isinstance(msg, Message)
+    assert msg.header is not None
+    assert len(msg.structures) == 1
+    assert isinstance(msg.structures[0], Codelist)
+    cl = msg.structures[0]
+    assert cl.id == "CL_FREQ"
+    assert cl.agency == "SDMX"
+    assert cl.version == "2.0"
+    assert len(cl.codes) == 9
+
+
+def test_get_json2_refmeta(sdmx_json_refmeta):
+    msg = read_sdmx(sdmx_json_refmeta)
+
+    assert isinstance(msg, Message)
+    assert msg.header is not None
+    assert len(msg.reports) == 1
+    assert isinstance(msg.reports[0], MetadataReport)
+    rep = msg.reports[0]
+    assert rep.id == "DTI_BIS_MACRO"
+    assert rep.agency == "BIS.MEDIT"
+    assert rep.version == "1.0.42"
+    assert len(rep.attributes) == 2
