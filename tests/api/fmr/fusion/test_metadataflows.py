63abd0388f0fe7424a369d8686321532b8c5fb91
Xavier Sosnovsky <1846109+sosna@users.noreply.github.com>
2025-10-03 15:04:31 +0200

    Add tests for deserialization of metadataflows

---
+++ tests/api/fmr/fusion/test_metadataflows.py
@@ -0,0 +1,45 @@
+import pytest
+
+import tests.api.fmr.metadataflow_checks as checks
+from pysdmx.api.fmr import AsyncRegistryClient, RegistryClient, StructureFormat
+
+
+@pytest.fixture
+def fmr() -> RegistryClient:
+    return RegistryClient(
+        "https://registry.sdmx.org/sdmx/v2",
+        StructureFormat.FUSION_JSON,
+    )
+
+
+@pytest.fixture
+def async_fmr() -> AsyncRegistryClient:
+    return AsyncRegistryClient(
+        "https://registry.sdmx.org/sdmx/v2/",
+        StructureFormat.FUSION_JSON,
+    )
+
+
+@pytest.fixture
+def query(fmr: RegistryClient) -> str:
+    res = "/structure/metadataflow"
+    all = "*"
+    latest = "+"
+    return f"{fmr.api_endpoint}{res}/{all}/{all}/{latest}"
+
+
+@pytest.fixture
+def body():
+    with open("tests/api/fmr/samples/refmeta/mdf.fusion.json", "rb") as f:
+        return f.read()
+
+
+def test_returns_dataflows(respx_mock, fmr, query, body):
+    """get_metadataflows() returns a collection of metadataflows."""
+    checks.check_metadataflows(respx_mock, fmr, query, body, True)
+
+
+@pytest.mark.asyncio
+async def test_returns_dataflows_async(respx_mock, async_fmr, query, body):
+    """get_metadataflows() return a collection of metadataflows (async)."""
+    await checks.check_metadataflows_async(respx_mock, async_fmr, query, body)
---
+++ tests/api/fmr/metadataflow_checks.py
@@ -0,0 +1,50 @@
+import httpx
+
+from pysdmx.api.fmr import AsyncRegistryClient, RegistryClient
+from pysdmx.io.format import Format
+from pysdmx.model import Metadataflow
+
+
+def check_metadataflows(
+    mock, fmr: RegistryClient, query, body, is_fusion: bool = False
+):
+    """get_metadataflows() returns a collection of metadataflows."""
+    mock.get(query).mock(
+        return_value=httpx.Response(
+            200,
+            content=body,
+        )
+    )
+
+    mdfs = fmr.get_metadataflows()
+
+    __check_metadataflows(mdfs)
+
+
+async def check_metadataflows_async(
+    mock, fmr: AsyncRegistryClient, query, body
+):
+    """get_metadataflows() returns a collection of metadataflows (async)."""
+    mock.get(query).mock(
+        return_value=httpx.Response(
+            200,
+            content=body,
+        )
+    )
+
+    mdfs = await fmr.get_metadataflows()
+
+    __check_metadataflows(mdfs)
+
+
+def __check_metadataflows(mdfs):
+    assert len(mdfs) == 1
+    mdf = mdfs[0]
+    assert isinstance(mdf, Metadataflow)
+    assert mdf.id == "DF_CNF"
+    assert mdf.agency == "TEST"
+    assert mdf.name == "Dataflow Configuration Settings"
+    assert mdf.version == "1.0"
+    assert "MetadataStructure=TEST:DF_CNF(1.0)" in mdf.structure
+    assert len(mdf.targets) == 1
+    assert "Dataflow=*:*(*)" in mdf.targets[0]
