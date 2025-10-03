0b713fe9a90428fb327217c4e90ab6128eedfe71
Xavier Sosnovsky <1846109+sosna@users.noreply.github.com>
2025-10-03 11:29:43 +0200

    Add helper methods to MSD

--- src/pysdmx/model/metadata.py
+++ src/pysdmx/model/metadata.py
@@ -21,6 +21,7 @@ from pysdmx.model.__base import (
 )
 from pysdmx.model.code import Codelist, Hierarchy
 from pysdmx.model.concept import Concept, DataType, Facets
+from pysdmx.model.dataflow import ArrayBoundaries
 from pysdmx.model.dataset import ActionType
 
 
@@ -66,12 +67,11 @@ class MetadataComponent(
     """
 
     is_presentational: bool = False
-    min_occurs: int = 1
-    max_occurs: int = 1
     concept: Union[Concept, ItemReference]
     local_dtype: Optional[DataType] = None
     local_facets: Optional[Facets] = None
     local_codes: Union[Codelist, Hierarchy, None] = None
+    array_def: Optional[ArrayBoundaries] = None
     local_enum_ref: Optional[str] = None
     components: Sequence["MetadataComponent"] = ()
 
@@ -175,6 +175,65 @@ class MetadataStructure(
     """
 
     components: Sequence[MetadataComponent] = ()
+
+    def __iter__(self) -> Iterator[MetadataComponent]:
+        """Return an iterator over the list of components."""
+        yield from self.components
+
+    def __len__(self) -> int:
+        """Return the number of components in the MSD."""
+        return self.__get_count(self.components)
+
+    def __getitem__(self, id_: str) -> Optional[MetadataComponent]:
+        """Return the component identified by the given ID."""
+        return self.__extract_cat(self.components, id_)
+
+    def __contains__(self, id_: str) -> bool:
+        """Whether there is a component with the supplied ID in the MSD."""
+        return bool(self.__getitem__(id_))
+
+    def __get_count(self, comps: Sequence[MetadataComponent]) -> int:
+        """Return the number of components at any levels."""
+        count = len(comps)
+        for comp in comps:
+            if comp.components:
+                count += self.__get_count(comp.components)
+        return count
+
+    def __extract_cat(
+        self, comps: Sequence[MetadataComponent], id_: str
+    ) -> Optional[MetadataComponent]:
+        if "." in id_:
+            ids = id_.split(".")
+            out = list(filter(lambda cat: cat.id == ids[0], comps))
+            if out:
+                pkey = ".".join(ids[1:])
+                return self.__extract_cat(out[0].components, pkey)
+        else:
+            out = list(filter(lambda cat: cat.id == id_, comps))
+            if out:
+                return out[0]
+        return None
+
+    def __str__(self) -> str:
+        """Custom string representation without the class name."""
+        processed_output = []
+        for attr, value, *_ in self.__rich_repr__():  # type: ignore[misc]
+            # str is taken as a Sequence, so we need to check it's not a str
+            if isinstance(value, Sequence) and not isinstance(value, str):
+                # Handle empty lists
+                if not value:
+                    continue
+                class_name = value[0].__class__.__name__
+                class_name = (
+                    class_name.lower() + "s"
+                    if attr != "components"
+                    else "components"
+                )
+                value = f"{len(value)} {class_name}"
+
+            processed_output.append(f"{attr}: {value}")
+        return f"{', '.join(processed_output)}"
 
 
 class MetadataAttribute(
