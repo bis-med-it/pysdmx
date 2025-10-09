4cfaaeac7f5657d8825fb3bebe2ae3fcd6210a86
Xavier Sosnovsky <1846109+sosna@users.noreply.github.com>
2025-10-09 11:31:20 +0200

    Add tests for basic metadata providers retrieval

---
+++ tests/api/fmr/fusion/test_metadata_providers.py
@@ -0,0 +1,66 @@
+import pytest
+
+import tests.api.fmr.metadata_provider_checks as checks
+from pysdmx.api.fmr import AsyncRegistryClient, RegistryClient, StructureFormat
+
+
+@pytest.fixture
+def fmr():
+    return RegistryClient(
+        "https://registry.sdmx.org/sdmx/v2",
+        StructureFormat.FUSION_JSON,
+    )
+
+
+@pytest.fixture
+def async_fmr():
+    return AsyncRegistryClient(
+        "https://registry.sdmx.org/sdmx/v2/",
+        StructureFormat.FUSION_JSON,
+    )
+
+
+@pytest.fixture
+def query(fmr: RegistryClient):
+    res = "/structure/metadataproviderscheme/"
+    agency = "BIS"
+    return f"{fmr.api_endpoint}{res}{agency}"
+
+
+@pytest.fixture
+def flowquery(fmr: RegistryClient):
+    res = "/structure/metadataproviderscheme/"
+    agency = "BIS"
+    return f"{fmr.api_endpoint}{res}{agency}?references=metadataprovisionagreement"
+
+
+@pytest.fixture
+def body():
+    with open(
+        "tests/api/fmr/samples/orgs/metadata_providers.fusion.json", "rb"
+    ) as f:
+        return f.read()
+
+
+# @pytest.fixture
+# def flowbody():
+#     with open(
+#         "tests/api/fmr/samples/orgs/metadataprovidersflows.fusion.json", "rb"
+#     ) as f:
+#         return f.read()
+
+
+def test_returns_metadata_providers(respx_mock, fmr, query, body):
+    """get_metadata_providers() returns a collection of organizations."""
+    checks.check_orgs(respx_mock, fmr, query, body)
+
+
+@pytest.mark.asyncio
+async def test_providers_have_core_info(respx_mock, async_fmr, query, body):
+    """Metadata providers contain core information such as ID, name, etc."""
+    await checks.check_org_core_info(respx_mock, async_fmr, query, body)
+
+
+# def test_providers_with_flows(respx_mock, fmr, flowquery, flowbody):
+#     """Providers may have a list of dataflows for which they provide data."""
+#     checks.check_with_flows(respx_mock, fmr, flowquery, flowbody)
---
+++ tests/api/fmr/metadata_provider_checks.py
@@ -0,0 +1,66 @@
+import httpx
+import pytest
+
+from pysdmx.api.fmr import AsyncRegistryClient, RegistryClient
+from pysdmx.io.format import Format
+from pysdmx.model import MetadataProvider
+
+
+def check_orgs(mock, fmr: RegistryClient, query, body):
+    """get_metadata_providers() should return metadata providers."""
+    mock.get(query).mock(
+        return_value=httpx.Response(
+            200,
+            content=body,
+        )
+    )
+
+    prvs = fmr.get_metadata_providers("BIS")
+
+    assert len(mock.calls) == 1
+
+    assert len(prvs) == 2
+    for prv in prvs:
+        assert isinstance(prv, MetadataProvider)
+
+
+async def check_org_core_info(mock, fmr: AsyncRegistryClient, query, body):
+    """Metadata providers contain core information such as ID, name, etc."""
+    mock.get(query).mock(
+        return_value=httpx.Response(
+            200,
+            content=body,
+        )
+    )
+
+    prvs = await fmr.get_metadata_providers("BIS")
+
+    for prv in prvs:
+        if prv.id == "MD":
+            assert prv.name == "Top-level metadata provider"
+        elif prv.id == "MD_PROVIDER":
+            assert prv.name == "Metadata Provider (test)"
+        else:
+            pytest.fail(f"Unexepcted provider: {prv.id}")
+
+
+# def check_with_flows(mock, fmr, query, body):
+#     """Providers may have flows attached."""
+#     mock.get(query).mock(
+#         return_value=httpx.Response(
+#             200,
+#             content=body,
+#         )
+#     )
+
+#     prvs = fmr.get_metadata_providers("BIS", True)
+
+#     for prv in prvs:
+#         if prv.id == "TEST":
+#             assert len(prv.dataflows) == 2
+#             for df in prv.dataflows:
+#                 assert df.id in ["DF1", "DF2"]
+#         elif prv.id == "TEST2":
+#             assert len(prv.dataflows) == 0
+#         else:
+#             pytest.fail(f"Unexepcted provider: {prv.id}")
