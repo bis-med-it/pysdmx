04c40c5765528f68dcbd232334067ae9da5ac959
ruizmaa <mario.cortes@meaningfuldata.eu>
2025-11-14 17:48:39 +0100

    refactor(tests: toolkit/vtl): use vtlengine's to_vtl_json for Schema to VTL conversion

--- src/pysdmx/toolkit/vtl/convert.py
+++ src/pysdmx/toolkit/vtl/convert.py
@@ -2,6 +2,10 @@
 
 from typing import Dict, Optional, Type
 
+from vtlengine.API import load_datasets  # type: ignore[import-untyped]
+from vtlengine.API._InternalApi import (  # type: ignore[import-untyped]
+    to_vtl_json,
+)
 from vtlengine.DataTypes import (  # type: ignore[import-untyped]
     Boolean,
     Date,
@@ -14,9 +18,8 @@ from vtlengine.DataTypes import (  # type: ignore[import-untyped]
     TimePeriod,
 )
 from vtlengine.Model import (  # type: ignore[import-untyped]
-    Component as VTLComponent,
+    Dataset as VTLengineDataset,
 )
-from vtlengine.Model import Dataset as VTLengineDataset
 from vtlengine.Model import Role as VTLRole
 
 from pysdmx.errors import Invalid
@@ -25,16 +28,7 @@ from pysdmx.model import Component, Components, Concept, Reference
 from pysdmx.model.concept import DataType
 from pysdmx.model.dataflow import Role, Schema
 
-SDMX_TO_VTL_TYPE_MAP: Dict[DataType, Type[ScalarType]] = {
-    DataType.STRING: String,
-    DataType.INTEGER: Integer,
-    DataType.DOUBLE: Number,
-    DataType.BOOLEAN: Boolean,
-    DataType.DATE: Date,         # GregorianDay
-    DataType.PERIOD: TimePeriod, # ObservationalTimePeriod
-    # DataType.TIME: Time,
-    DataType.DURATION: Duration,
-}
+# VTL to SDMX type mapping
 VTL_TO_SDMX_TYPE_MAP: Dict[Type[ScalarType], DataType] = {
     String: DataType.STRING,
     Integer: DataType.INTEGER,
@@ -46,12 +40,7 @@ VTL_TO_SDMX_TYPE_MAP: Dict[Type[ScalarType], DataType] = {
     Duration: DataType.DURATION,
 }
 
-# Role mappings
-SDMX_TO_VTL_ROLE_MAP: Dict[Role, VTLRole] = {
-    Role.DIMENSION: VTLRole.IDENTIFIER,
-    Role.MEASURE: VTLRole.MEASURE,
-    Role.ATTRIBUTE: VTLRole.ATTRIBUTE,
-}
+# Role mapping
 VTL_TO_SDMX_ROLE_MAP: Dict[VTLRole, Role] = {
     VTLRole.IDENTIFIER: Role.DIMENSION,
     VTLRole.MEASURE: Role.MEASURE,
@@ -87,59 +76,15 @@ def convert_dataset_to_vtl(
     schema = dataset.structure
     pd_dataset = dataset.data
 
-    # Get dataframe columns if data is provided
-    if pd_dataset is not None:
-        pd_dataset_columns = set(pd_dataset.columns)
-    else:
-        pd_dataset_columns = set()
+    # Use vtlengine's built-in conversion function to convert Schema to VTL
+    vtl_json = to_vtl_json(schema, vtl_dataset_name)
 
-    # Convert components from SDMX to VTL format
-    vtl_components: Dict[str, VTLComponent] = {}
-    for component in schema.components:
-        component_id = str(component.id)
+    # Load the dataset structure using vtlengine's API
+    datasets = load_datasets(vtl_json)
+    vtl_dataset = datasets[vtl_dataset_name]
 
-        # Ensure component column exists in dataframe (if data provided)
-        if pd_dataset_columns and component_id not in pd_dataset_columns:
-            raise Invalid(
-                "Validation Error",
-                f"Component '{component_id}' defined in Schema not found "
-                "in dataset.data columns",
-            )
-
-        # Map the data type
-        sdmx_dtype = component.dtype
-        if sdmx_dtype not in SDMX_TO_VTL_TYPE_MAP:
-            supported = ", ".join(str(t) for t in SDMX_TO_VTL_TYPE_MAP)
-            raise Invalid(
-                "Validation Error",
-                f"SDMX DataType '{sdmx_dtype}' cannot be mapped to a "
-                f"VTL type. Supported types are: {supported}",
-            )
-        vtl_dtype = SDMX_TO_VTL_TYPE_MAP[sdmx_dtype]
-
-        # Map the role
-        if component.role not in SDMX_TO_VTL_ROLE_MAP:
-            raise Invalid(
-                "Validation Error",
-                f"SDMX Role '{component.role}' cannot be mapped to a VTL Role",
-            )
-        vtl_role = SDMX_TO_VTL_ROLE_MAP[component.role]
-
-        # Create VTL component (nullable is the opposite of required)
-        vtl_comp = VTLComponent(
-            name=component_id,
-            data_type=vtl_dtype,
-            role=vtl_role,
-            nullable=not getattr(component, "required", False),
-        )
-        vtl_components[component_id] = vtl_comp
-
-    # Create the vtlengine Dataset
-    vtl_dataset = VTLengineDataset(
-        name=vtl_dataset_name,
-        components=vtl_components,
-        data=pd_dataset
-    )
+    # Assign the pandas DataFrame to the VTL dataset
+    vtl_dataset.data = pd_dataset
 
     return vtl_dataset
 
