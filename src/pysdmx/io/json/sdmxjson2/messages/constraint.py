08be7f37462f6d86d023efd5bbc853a42e12aaa9
Xavier Sosnovsky <1846109+sosna@users.noreply.github.com>
2025-11-14 14:14:38 +0100

    Add the various to_model methods

--- src/pysdmx/io/json/sdmxjson2/messages/constraint.py
+++ src/pysdmx/io/json/sdmxjson2/messages/constraint.py
@@ -6,15 +6,28 @@ from typing import Dict, Literal, Optional, Sequence
 from msgspec import Struct
 
 from pysdmx.io.json.sdmxjson2.messages.core import MaintainableType
+from pysdmx.model import (
+    ConstraintAttachment,
+    CubeKeyValue,
+    CubeRegion,
+    CubeValue,
+    DataConstraint,
+    DataKey,
+    DataKeyValue,
+    KeySet,
+)
 
 
 class JsonValue(Struct, frozen=True, omit_defaults=True):
-    """SDMX-JSON payload for an allowed value."""
+    """SDMX-JSON payload for a cube value."""
 
     value: str
-    cascadeValues: bool = False
     validFrom: Optional[datetime] = None
     validTo: Optional[datetime] = None
+
+    def to_model(self) -> CubeValue:
+        """Returns the requested cube value."""
+        return CubeValue(self.value, self.validFrom, self.validTo)
 
 
 class JsonKeyValue(Struct, frozen=True, omit_defaults=True):
@@ -26,9 +39,9 @@ class JsonKeyValue(Struct, frozen=True, omit_defaults=True):
     # removePrefix, validFrom, validTo, timeRange) but not by the FMR. Therefore,
     # they are ignored for now.
 
-    def to_model(self) -> Sequence[str]:
+    def to_model(self) -> CubeKeyValue:
         """Returns the requested list of values."""
-        return [v.value for v in self.values]
+        return CubeKeyValue(self.id, [v.to_model() for v in self.values])
 
 
 class JsonCubeRegion(Struct, frozen=True, omit_defaults=True):
@@ -38,9 +51,11 @@ class JsonCubeRegion(Struct, frozen=True, omit_defaults=True):
     keyValues: Sequence[JsonKeyValue]
     include: bool = True
 
-    def to_map(self) -> Dict[str, Sequence[str]]:
-        """Gets the list of allowed values for a component."""
-        return {kv.id: kv.to_model() for kv in self.keyValues}
+    def to_model(self) -> CubeKeyValue:
+        """Returns the requested list of values."""
+        return CubeRegion(
+            [kv.to_model() for kv in self.keyValues], self.include
+        )
 
 
 class JsonConstraintAttachment(Struct, frozen=True, omit_defaults=True):
@@ -51,12 +66,23 @@ class JsonConstraintAttachment(Struct, frozen=True, omit_defaults=True):
     dataflows: Optional[Sequence[str]] = None
     provisionAgreements: Optional[Sequence[str]] = None
 
+    def to_model(self) -> ConstraintAttachment:
+        return ConstraintAttachment(
+            self.dataProvider,
+            self.dataStructures,
+            self.dataflows,
+            self.provisionAgreements,
+        )
+
 
 class JsonDataKeyValue(Struct, frozen=True, omit_defaults=True):
     """SDMX-JSON payload for a data key value."""
 
     id: str
     value: str
+
+    def to_model(self) -> DataKeyValue:
+        return DataKeyValue(self.id, self.value)
 
 
 class JsonDataKey(Struct, frozen=True, omit_defaults=True):
@@ -66,6 +92,13 @@ class JsonDataKey(Struct, frozen=True, omit_defaults=True):
     validFrom: Optional[datetime] = None
     validTo: Optional[datetime] = None
 
+    def to_model(self) -> DataKey:
+        return DataKey(
+            [kv.to_model() for kv in self.keysValues],
+            self.validFrom,
+            self.validTo,
+        )
+
 
 class JsonKeySet(Struct, frozen=True, omit_defaults=True):
     """SDMX-JSON payload for a keyset."""
@@ -73,11 +106,52 @@ class JsonKeySet(Struct, frozen=True, omit_defaults=True):
     isIncluded: bool
     keys: Sequence[JsonDataKey]
 
+    def to_model(self) -> DataKey:
+        return KeySet([k.to_model() for k in self.keys], self.isIncluded)
+
 
 class JsonDataConstraint(MaintainableType, frozen=True, omit_defaults=True):
     """SDMX-JSON payload for a content constraint."""
 
     role: Optional[Literal["Allowed", "Actual"]] = None
     constraintAttachment: Optional[JsonConstraintAttachment] = None
-    cubeRegions: Optional[Sequence[JsonCubeRegion]] = None
-    dataKeySets: Optional[Sequence[JsonKeySet]] = None
+    cubeRegions: Sequence[JsonCubeRegion] = ()
+    dataKeySets: Sequence[JsonKeySet] = ()
+
+    def to_model(self) -> DataConstraint:
+        """Converts a JsonDataConstraint to a pysdmx Data Constraint."""
+        return DataConstraint(
+            id=self.id,
+            name=self.name,
+            agency=self.agency,
+            description=self.description,
+            version=self.version,
+            annotations=tuple([a.to_model() for a in self.annotations]),
+            is_external_reference=self.isExternalReference,
+            valid_from=self.validFrom,
+            valid_to=self.validTo,
+            role=self.role,
+            constraint_attachment=self.constraintAttachment.to_model(),
+            cube_regions=[r.to_model() for r in self.cubeRegions],
+            key_sets=[s.to_model() for s in self.dataKeySets],
+        )
+
+
+class JsonDataConstraints(Struct, frozen=True, omit_defaults=True):
+    """SDMX-JSON payload for data constraints."""
+
+    contentConstraints: Sequence[JsonDataConstraint] = ()
+
+    def to_model(self) -> Sequence[DataConstraint]:
+        """Returns the requested data constraints."""
+        return [cc.to_model() for cc in self.contentConstraints]
+
+
+class JsonDataConstraintMessage(Struct, frozen=True, omit_defaults=True):
+    """SDMX-JSON payload for /dataconstraint queries."""
+
+    data: JsonDataConstraints
+
+    def to_model(self) -> Sequence[DataConstraint]:
+        """Returns the requested data constraints."""
+        return self.data.to_model()
