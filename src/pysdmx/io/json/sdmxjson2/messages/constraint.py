c0d59c50eb5b73e3f20ce7c937729db47845d039
Xavier Sosnovsky <1846109+sosna@users.noreply.github.com>
2025-11-14 12:58:32 +0100

    Extend JSON model for constraints

--- src/pysdmx/io/json/sdmxjson2/messages/constraint.py
+++ src/pysdmx/io/json/sdmxjson2/messages/constraint.py
@@ -1,5 +1,6 @@
 """Collection of SDMX-JSON schemas for content constraints."""
 
+from datetime import datetime
 from typing import Dict, Literal, Optional, Sequence
 
 from msgspec import Struct
@@ -11,6 +12,9 @@ class JsonValue(Struct, frozen=True, omit_defaults=True):
     """SDMX-JSON payload for an allowed value."""
 
     value: str
+    cascadeValues: bool = False
+    validFrom: Optional[datetime] = None
+    validTo: Optional[datetime] = None
 
 
 class JsonKeyValue(Struct, frozen=True, omit_defaults=True):
@@ -18,6 +22,9 @@ class JsonKeyValue(Struct, frozen=True, omit_defaults=True):
 
     id: str
     values: Sequence[JsonValue]
+    # Additional properties are supported in the model (include,
+    # removePrefix, validFrom, validTo, timeRange) but not by the FMR. Therefore,
+    # they are ignored for now.
 
     def to_model(self) -> Sequence[str]:
         """Returns the requested list of values."""
@@ -27,7 +34,9 @@ class JsonKeyValue(Struct, frozen=True, omit_defaults=True):
 class JsonCubeRegion(Struct, frozen=True, omit_defaults=True):
     """SDMX-JSON payload for a cube region."""
 
+    # The property `components` is ignored as it's not used in the FMR`
     keyValues: Sequence[JsonKeyValue]
+    include: bool = True
 
     def to_map(self) -> Dict[str, Sequence[str]]:
         """Gets the list of allowed values for a component."""
@@ -38,11 +47,31 @@ class JsonConstraintAttachment(Struct, frozen=True, omit_defaults=True):
     """SDMX-JSON payload for a constraint attachment."""
 
     dataProvider: Optional[str]
-    simpleDataSources: Optional[Sequence[str]] = None
     dataStructures: Optional[Sequence[str]] = None
     dataflows: Optional[Sequence[str]] = None
     provisionAgreements: Optional[Sequence[str]] = None
-    queryableDataSources: Optional[Sequence[str]] = None
+
+
+class JsonDataKeyValue(Struct, frozen=True, omit_defaults=True):
+    """SDMX-JSON payload for a data key value."""
+
+    id: str
+    value: str
+
+
+class JsonDataKey(Struct, frozen=True, omit_defaults=True):
+    """SDMX-JSON payload for a data key."""
+
+    keysValues: Sequence[JsonDataKeyValue]
+    validFrom: Optional[datetime] = None
+    validTo: Optional[datetime] = None
+
+
+class JsonKeySet(Struct, frozen=True, omit_defaults=True):
+    """SDMX-JSON payload for a keyset."""
+
+    isIncluded: bool
+    keys: Sequence[JsonDataKey]
 
 
 class JsonDataConstraint(MaintainableType, frozen=True, omit_defaults=True):
@@ -51,3 +80,4 @@ class JsonDataConstraint(MaintainableType, frozen=True, omit_defaults=True):
     role: Optional[Literal["Allowed", "Actual"]] = None
     constraintAttachment: Optional[JsonConstraintAttachment] = None
     cubeRegions: Optional[Sequence[JsonCubeRegion]] = None
+    dataKeySets: Optional[Sequence[JsonKeySet]] = None
