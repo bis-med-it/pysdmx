8c56eb3916266dd133c271bf315b6670f26a9736
ruizmaa <mario.cortes@meaningfuldata.eu>
2025-12-01 18:18:15 +0100

    fix(tests/csv):align CSV writer schemas with input data columns

--- tests/io/csv/sdmx21/writer/test_writer_v21.py
+++ tests/io/csv/sdmx21/writer/test_writer_v21.py
@@ -170,17 +170,129 @@ def schema_provision_agreement():
         components=Components(
             [
                 Component(
-                    id="DIM1",
+                    id="FREQ",
                     concept=Concept(
-                        id="DIM1", name="DIMENSION 1", dtype=DataType.STRING
+                        id="FREQ", name="FREQ", dtype=DataType.STRING
                     ),
                     role=Role.DIMENSION,
                     required=True,
                 ),
                 Component(
-                    id="DIM2",
+                    id="DER_TYPE",
                     concept=Concept(
-                        id="DIM2", name="DIMENSION 2", dtype=DataType.STRING
+                        id="DER_TYPE", name="DER_TYPE", dtype=DataType.STRING
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_INSTR",
+                    concept=Concept(
+                        id="DER_INSTR", name="DER_INSTR", dtype=DataType.STRING
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_RISK",
+                    concept=Concept(
+                        id="DER_RISK", name="DER_RISK", dtype=DataType.STRING
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_REP_CTY",
+                    concept=Concept(
+                        id="DER_REP_CTY",
+                        name="DER_REP_CTY",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_SECTOR_CPY",
+                    concept=Concept(
+                        id="DER_SECTOR_CPY",
+                        name="DER_SECTOR_CPY",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_CPC",
+                    concept=Concept(
+                        id="DER_CPC", name="DER_CPC", dtype=DataType.STRING
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_SECTOR_UDL",
+                    concept=Concept(
+                        id="DER_SECTOR_UDL",
+                        name="DER_SECTOR_UDL",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_CURR_LEG1",
+                    concept=Concept(
+                        id="DER_CURR_LEG1",
+                        name="DER_CURR_LEG1",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_CURR_LEG2",
+                    concept=Concept(
+                        id="DER_CURR_LEG2",
+                        name="DER_CURR_LEG2",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_ISSUE_MAT",
+                    concept=Concept(
+                        id="DER_ISSUE_MAT",
+                        name="DER_ISSUE_MAT",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_RATING",
+                    concept=Concept(
+                        id="DER_RATING",
+                        name="DER_RATING",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_EX_METHOD",
+                    concept=Concept(
+                        id="DER_EX_METHOD",
+                        name="DER_EX_METHOD",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_BASIS",
+                    concept=Concept(
+                        id="DER_BASIS", name="DER_BASIS", dtype=DataType.STRING
                     ),
                     role=Role.DIMENSION,
                     required=True,
@@ -196,30 +308,54 @@ def schema_provision_agreement():
                     required=True,
                 ),
                 Component(
-                    id="ATT1",
-                    concept=Concept(
-                        id="ATT1", name="ATTRIBUTE 1", dtype=DataType.STRING
-                    ),
-                    role=Role.ATTRIBUTE,
-                    required=False,
-                    attachment_level="S",
-                ),
-                Component(
-                    id="ATT2",
-                    concept=Concept(
-                        id="ATT2", name="ATTRIBUTE 2", dtype=DataType.STRING
-                    ),
-                    role=Role.ATTRIBUTE,
-                    required=False,
-                    attachment_level="O",
-                ),
-                Component(
                     id="OBS_VALUE",
                     concept=Concept(
                         id="OBS_VALUE", name="OBS_VALUE", dtype=DataType.STRING
                     ),
                     role=Role.MEASURE,
                     required=False,
+                ),
+                Component(
+                    id="AVAILABILITY",
+                    concept=Concept(
+                        id="AVAILABILITY",
+                        name="AVAILABILITY",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.ATTRIBUTE,
+                    required=False,
+                    attachment_level="O",
+                ),
+                Component(
+                    id="COLLECTION",
+                    concept=Concept(
+                        id="COLLECTION",
+                        name="COLLECTION",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.ATTRIBUTE,
+                    required=False,
+                    attachment_level="O",
+                ),
+                Component(
+                    id="OBS_STATUS",
+                    concept=Concept(
+                        id="OBS_STATUS",
+                        name="OBS_STATUS",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.ATTRIBUTE,
+                    required=False,
+                    attachment_level="O",
+                ),
+                Component(
+                    id="OBS_CONF",
+                    concept=Concept(
+                        id="OBS_CONF", name="OBS_CONF", dtype=DataType.STRING
+                    ),
+                    role=Role.ATTRIBUTE,
+                    required=False,
+                    attachment_level="O",
                 ),
             ]
         ),
@@ -237,17 +373,129 @@ def schema_alter_ID():
         components=Components(
             [
                 Component(
-                    id="DIM1",
+                    id="FREQ",
                     concept=Concept(
-                        id="DIM1", name="DIMENSION 1", dtype=DataType.STRING
+                        id="FREQ", name="FREQ", dtype=DataType.STRING
                     ),
                     role=Role.DIMENSION,
                     required=True,
                 ),
                 Component(
-                    id="DIM2",
+                    id="DER_TYPE",
                     concept=Concept(
-                        id="DIM2", name="DIMENSION 2", dtype=DataType.STRING
+                        id="DER_TYPE", name="DER_TYPE", dtype=DataType.STRING
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_INSTR",
+                    concept=Concept(
+                        id="DER_INSTR", name="DER_INSTR", dtype=DataType.STRING
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_RISK",
+                    concept=Concept(
+                        id="DER_RISK", name="DER_RISK", dtype=DataType.STRING
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_REP_CTY",
+                    concept=Concept(
+                        id="DER_REP_CTY",
+                        name="DER_REP_CTY",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_SECTOR_CPY",
+                    concept=Concept(
+                        id="DER_SECTOR_CPY",
+                        name="DER_SECTOR_CPY",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_CPC",
+                    concept=Concept(
+                        id="DER_CPC", name="DER_CPC", dtype=DataType.STRING
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_SECTOR_UDL",
+                    concept=Concept(
+                        id="DER_SECTOR_UDL",
+                        name="DER_SECTOR_UDL",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_CURR_LEG1",
+                    concept=Concept(
+                        id="DER_CURR_LEG1",
+                        name="DER_CURR_LEG1",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_CURR_LEG2",
+                    concept=Concept(
+                        id="DER_CURR_LEG2",
+                        name="DER_CURR_LEG2",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_ISSUE_MAT",
+                    concept=Concept(
+                        id="DER_ISSUE_MAT",
+                        name="DER_ISSUE_MAT",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_RATING",
+                    concept=Concept(
+                        id="DER_RATING",
+                        name="DER_RATING",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_EX_METHOD",
+                    concept=Concept(
+                        id="DER_EX_METHOD",
+                        name="DER_EX_METHOD",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.DIMENSION,
+                    required=True,
+                ),
+                Component(
+                    id="DER_BASIS",
+                    concept=Concept(
+                        id="DER_BASIS", name="DER_BASIS", dtype=DataType.STRING
                     ),
                     role=Role.DIMENSION,
                     required=True,
@@ -263,30 +511,54 @@ def schema_alter_ID():
                     required=True,
                 ),
                 Component(
-                    id="ATT1",
-                    concept=Concept(
-                        id="ATT1", name="ATTRIBUTE 1", dtype=DataType.STRING
-                    ),
-                    role=Role.ATTRIBUTE,
-                    required=False,
-                    attachment_level="S",
-                ),
-                Component(
-                    id="ATT2",
-                    concept=Concept(
-                        id="ATT2", name="ATTRIBUTE 2", dtype=DataType.STRING
-                    ),
-                    role=Role.ATTRIBUTE,
-                    required=False,
-                    attachment_level="O",
-                ),
-                Component(
                     id="OBS_VALUE",
                     concept=Concept(
                         id="OBS_VALUE", name="OBS_VALUE", dtype=DataType.STRING
                     ),
                     role=Role.MEASURE,
                     required=False,
+                ),
+                Component(
+                    id="AVAILABILITY",
+                    concept=Concept(
+                        id="AVAILABILITY",
+                        name="AVAILABILITY",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.ATTRIBUTE,
+                    required=False,
+                    attachment_level="O",
+                ),
+                Component(
+                    id="COLLECTION",
+                    concept=Concept(
+                        id="COLLECTION",
+                        name="COLLECTION",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.ATTRIBUTE,
+                    required=False,
+                    attachment_level="O",
+                ),
+                Component(
+                    id="OBS_STATUS",
+                    concept=Concept(
+                        id="OBS_STATUS",
+                        name="OBS_STATUS",
+                        dtype=DataType.STRING,
+                    ),
+                    role=Role.ATTRIBUTE,
+                    required=False,
+                    attachment_level="O",
+                ),
+                Component(
+                    id="OBS_CONF",
+                    concept=Concept(
+                        id="OBS_CONF", name="OBS_CONF", dtype=DataType.STRING
+                    ),
+                    role=Role.ATTRIBUTE,
+                    required=False,
+                    attachment_level="O",
                 ),
             ]
         ),
