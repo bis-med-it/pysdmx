778471b350d1d5483c03a932dd195197a1ef61d7
Xavier Sosnovsky <1846109+sosna@users.noreply.github.com>
2026-03-26 10:21:51 +0100

    Add MaintainableIdentification to dc API

--- src/pysdmx/api/dc/__init__.py
+++ src/pysdmx/api/dc/__init__.py
@@ -1,6 +1,15 @@
 """pysdmx simple data discovery and retrieval API."""
 
-from pysdmx.api.dc._api import BasicConnector, Connector
+from pysdmx.api.dc._api import (
+    BasicConnector,
+    Connector,
+    MaintainableIdentification,
+)
 from pysdmx.api.dc._rest import SdmxConnector
 
-__all__ = ["BasicConnector", "Connector", "SdmxConnector"]
+__all__ = [
+    "BasicConnector",
+    "Connector",
+    "MaintainableIdentification",
+    "SdmxConnector",
+]
