5516e2582ee97dff7fe5ac6293fff4bc06a53b5f
Xavier Sosnovsky <1846109+sosna@users.noreply.github.com>
2026-04-16 14:22:32 +0200

    Add tests for data method of SdmxConnector

--- tests/api/dc/test_rest_client.py
+++ tests/api/dc/test_rest_client.py
@@ -37,6 +37,12 @@ def json_availability():
 
 
 @pytest.fixture
+def csv_data():
+    with open("tests/io/csv/sdmx20/reader/samples/data_v2.csv", "rb") as f:
+        return f.read()
+
+
+@pytest.fixture
 def query_flows(host):
     return f"{host}/structure/dataflow?detail=allcompletestubs"
 
@@ -44,6 +50,14 @@ def query_flows(host):
 @pytest.fixture
 def query_availability(host):
     return f"{host}/availability/dataflow/BIS/CBS/1.0?references=all"
+
+
+@pytest.fixture
+def query_data(host):
+    return (
+        f"{host}/data/dataflow/BIS/BIS_DER/1.0"
+        "?dimensionAtObservation=AllDimensions"
+    )
 
 
 def test_list_dataflows(respx_mock, rest_client, query_flows, json_flows):
@@ -158,3 +172,42 @@ def test_availability_deser_issues(
 
     with pytest.raises(InternalError):
         rest_client.dataflow(dfref)
+
+
+def test_data_query(respx_mock, rest_client, query_data, csv_data):
+    respx_mock.get(query_data).mock(
+        return_value=httpx.Response(200, content=csv_data)
+    )
+    dfref = DataflowRef("BIS", "BIS_DER", "1.0")
+
+    data = list(rest_client.data(dfref))
+
+    assert len(data) == 20
+    for row in data:
+        assert isinstance(row, dict)
+
+
+def test_data_query_dfstr(respx_mock, rest_client, query_data, csv_data):
+    respx_mock.get(query_data).mock(
+        return_value=httpx.Response(200, content=csv_data)
+    )
+    dfref = (
+        "urn:sdmx:org.sdmx.infomodel.datastructure.Dataflow=BIS:BIS_DER(1.0)"
+    )
+
+    data = list(rest_client.data(dfref))
+
+    assert len(data) == 20
+    for row in data:
+        assert isinstance(row, dict)
+
+
+def test_data_nf(respx_mock, rest_client, query_data):
+    respx_mock.get(query_data).mock(return_value=httpx.Response(404))
+    dfref = (
+        "urn:sdmx:org.sdmx.infomodel.datastructure.Dataflow=BIS:BIS_DER(1.0)"
+    )
+
+    with pytest.raises(NotFound):
+        for _ in rest_client.data(dfref):
+            pass
