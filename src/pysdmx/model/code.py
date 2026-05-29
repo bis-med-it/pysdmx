ef2f54c34a1ccc76c7230d1fde32c5a11e11ef42
Xavier Sosnovsky <1846109+sosna@users.noreply.github.com>
2026-05-29 13:43:57 +0200

    Limit DataType and Facets to type checking only

--- src/pysdmx/model/code.py
+++ src/pysdmx/model/code.py
@@ -17,7 +17,7 @@ representation of hierarchical relationships to hierarchies only.
 """
 
 from datetime import datetime
-from typing import Iterator, Literal, Optional, Sequence, Union
+from typing import TYPE_CHECKING, Iterator, Literal, Optional, Sequence, Union
 
 from msgspec import Struct
 
@@ -29,7 +29,9 @@ from pysdmx.model.__base import (
     MaintainableArtefact,
     NameableArtefact,
 )
-from pysdmx.model.concept import DataType, Facets
+
+if TYPE_CHECKING:
+    from pysdmx.model.concept import DataType, Facets
 
 
 class Code(Item, frozen=True, omit_defaults=True):
@@ -212,8 +214,8 @@ class LevelType(NameableArtefact, frozen=True, omit_defaults=True):
     """
 
     level: Optional["LevelType"] = None
-    dtype: Optional[DataType] = None
-    facets: Optional[Facets] = None
+    dtype: Optional["DataType"] = None
+    facets: Optional["Facets"] = None
 
 
 class Hierarchy(
